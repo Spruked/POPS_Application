a41b9f14076c54b1d76b67a1912b950926e4e954
Bryan Anthony Spruk <bryanspruk7@outlook.com>
2026-06-27 03:28:46 -0500

    Keep Pops assistant fitted to full desktop window

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./styles/pops-assistant.css";
+import "./styles/pops-assistant-fit.css";
 import Sidebar, { APP_NAV_LABELS } from "./components/Sidebar";
 import ToastContainer from "./components/ToastContainer";
 import PopsAssistant from "./components/PopsAssistant";
